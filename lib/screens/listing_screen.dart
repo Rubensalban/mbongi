242771b095757eed663b816e070a46d2039db7d0
rubensalban <rubensalban@gmail.com>
2022-10-02 14:36:52 +0100

    test 2

---
+++ lib/screens/listing_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ListingScreen extends StatelessWidget {
+  const ListingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
